68168bb58abc558ffc15e3071a62df8b4d93b561
allanlechineskisilva-rgb <allan.lechineski.silva@escola.pr.gov.br>
2026-08-06 12:14:39 -0300

    ok

---
+++ script.js
@@ -0,0 +1,8 @@
+ //botão curtida
+
+ document.addEventListener("DOMContentLoaded",)() =>{
+    const likeBtn = document.querySelector(".left-actions .action-btn:firts-child");
+ if(!likeBtn) return;
+ const likeSvg = likeBtn.querySelector("svg")
+
+ }

49d8aa01442cbd487cbbaf078c737c475e48ce94
allanlechineskisilva-rgb <allan.lechineski.silva@escola.pr.gov.br>
2026-09-03 11:41:25 -0300

    ok

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
- //botão curtida
+//botao curtida
 
- document.addEventListener("DOMContentLoaded",)() =>{
-    const likeBtn = document.querySelector(".left-actions .action-btn:firts-child");
- if(!likeBtn) return;
- const likeSvg = likeBtn.querySelector("svg")
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".left-actions .action-btn:first-child");
+    if (!likeBtn) return;
+    const likeSvg =likeBtn.querySelector("svg");
 
 
-   //coraçao
-   funcion applyLikedsyle (){
-   likeSvg.style.fill =
-   likeSvg.style.stock
-   likeSvg.style.fill
+    //localiza o contador
 
-   efeito curtida 
-   likeSvg.style.tansform = "scale(1.2)";
-   setTimeout(() => (likeSvg.style.transform="scate(1)")1;
-      
-   
- }
+    let textNode = Array.from(likeBtn.childNodes).find(node) => node.nodeType
+    ===node.TEXT-NODE && node.textContent.trim() !== ""
+)
+
+//zera o contador
+let cont = 0;
+
+//atualiza
+if(textNode){
+textNode.textContent = '0';
+}
+
+//coração
+function applyLikedStyle (){
+likeSvg.style.fill = "#ef4444";
+likeSvg.style.stroke = "#ef4444";
+likeSvg.style.color = "#ef4444";
+}
+
+//efito curtida
+likeSvg.style.transform = "scale(1.3)";
+setTimeout (() => (likeSvg.style.transform = "scal(1)")150);
+
+//para numerros de 1000
+
+function formatLikes(num) {
+  if(num >=100){
+        return(num/1000),toFixed(1)+"k";
+}
